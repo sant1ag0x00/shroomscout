f9b0e5b0e3725edf576cb45124c40bac7098a53f
Lauritz.Kremzow@telekom.de <Lauritz.Kremzow@telekom.de>
2024-01-21 15:39:36 +0100

    #5: Implement register screen

--- frontend/shroomscout/src/app/app.module.ts
+++ frontend/shroomscout/src/app/app.module.ts
@@ -3,20 +3,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MapComponent } from './map/map.component';
-import { RegisterComponent } from './register/register.component';
 import { LiveFeedComponent } from './live-feed/live-feed.component';
+import { MapComponent } from './map/map.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, MapComponent, RegisterComponent, LiveFeedComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MapComponent,
+    RegisterComponent,
+    LiveFeedComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     MatToolbarModule,
     MatButtonModule,
   ],
--- frontend/shroomscout/src/app/register/register.component.ts
+++ frontend/shroomscout/src/app/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  title: string = 'Register a Mushroom:';
+  name: string = '';
+  environments: string[] = ['Wiese', 'Eiche', 'Buche'];
+  selectedEnvironment: string = '';
+  pictureUrl: string = '';
+
+  onRegisterClick() {
+    this.router.navigate(['']);
+  }
 }
