07126df08ea5dfde4e041047a001b02a4cdf0697
Lauritz.Kremzow@telekom.de <Lauritz.Kremzow@telekom.de>
2024-01-30 16:34:43 +0100

    #13: Try to use leaflet

--- frontend/shroomscout/src/app/map/map.component.ts
+++ frontend/shroomscout/src/app/map/map.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  private map!: L.Map;
 
+  ngOnInit() {
+    this.map = L.map('map').setView([52.52, 13.405], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 15,
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this.map);
+
+    this.map.invalidateSize();
+  }
 }
