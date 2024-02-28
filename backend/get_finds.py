aa2931903a1a4df8d666d0e589b68928ec687b15
Sant1ag0x00 <tim@weltraumkatzen.space>
2024-02-28 15:40:45 +0100

    fix json format in get_finds

--- backend/get_finds.py
+++ backend/get_finds.py
@@ -6,10 +6,15 @@ def get_finds():
     cur = con.cursor()
     cur.execute("SELECT * FROM findLog")
     rows = cur.fetchall()
+    # Fetch the column names from the cursor
+    columns = [column[0] for column in cur.description]
     for row in rows:
-        findlist.append(list(row)) 
-    con.close() 
+        # Create a dictionary for each row with column names as keys
+        row_dict = dict(zip(columns, row))
+        findlist.append(row_dict)
+    con.close()
     return findlist
+
 
 
 if __name__ == "__main__":
